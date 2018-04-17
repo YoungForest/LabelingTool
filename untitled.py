1dd5dde85dc3bbc521090032a609aecf5b96d6c7
YoungForest <yangsen758@foxmail.com>
2018-04-17 13:53:13 +0800

    video page init

--- untitled.py
+++ untitled.py
@@ -1,25 +1,37 @@
-from flask import Flask, session, redirect, url_for, request
+from flask import Flask, session, redirect, url_for, request, render_template, send_file
 import glob
 import os
 
 app = Flask(__name__)
 app.debug = True
 video_path = 'A:/su/cutresults/'
-videos = None
-videos_name = None
+expansion = 'flv'
+videos = glob.glob(video_path + "/*." + expansion)
+videos_name = []
+content = ''
+name2path = {}
+for v in videos:
+    name = os.path.basename(v)
+    filename, file_extension = os.path.splitext(name)
+    content += '<li><a href="/%s">%s</a></li>' %(filename, filename)
+    videos_name.append(filename)
+    name2path[filename] = v.replace('\\', '/')
 
 @app.route('/')
 def index():
-    expansion = 'flv'
-    videos = glob.glob(video_path + "/*." + expansion)
-    videos_name = []
-    content = ''
-    for v in videos:
-        name = os.path.basename(v)
-        filename, file_extension = os.path.splitext(name)
-        content += '<li><a href="/%s">%s</a></li>' %(filename, filename)
-        videos_name.append(filename)
     return '<ol>' + content + '</ol>'
+
+@app.route('/<name>')
+def label_page(name=None):
+    return render_template('index.html', name=name)
+
+@app.route('/video/<name>')
+def video_file_server(name=None):
+    path = name2path[name]
+    resp = send_file(path, mimetype='video/x-flv')
+
+    return resp
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
